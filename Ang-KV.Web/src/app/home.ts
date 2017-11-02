3d141e55caccba70ccb73bc8f5d958fab4c6cbfb
Naresh-U <tech.nareshworks@gmail.com>
2017-11-01 21:31:50 -0400

    adds image using property binding

--- Ang-KV.Web/src/app/home.ts
+++ Ang-KV.Web/src/app/home.ts
@@ -2,9 +2,10 @@
 
 @Component({
     selector: 'home',
-    template:`<h1>Home Component</h1><br><h4>{{name}}</h4>`
+    template:`<h1>Home Component</h1><br><h4>{{name}}</h4><img [src]='imagePath'/>`
 })
 export class HomeComponent
 {
     name = 'Kar';
+    imagePath: string = "http://www.stylisheve.com/wp-content/uploads/2012/01/Hairstyles-for-over-50-men_10.jpg";
 }
